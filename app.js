43248fddfd0ae00f5537a9442e34719143a27559
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-01-29 15:02:45 -0300

    feat: controle de estoque

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
     mensagemAlerta: 'Item adicionado',
     alertaAtivo: false,
   },
@@ -48,6 +49,18 @@ const vm = new Vue({
         this.produto = false;
       }
     },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.carrinhoAtivo = false;
+      }
+    },
     adicionarItem() {
       this.produto.estoque--;
       const { id, nome, preco } = this.produto;
@@ -61,6 +74,10 @@ const vm = new Vue({
       if (window.localStorage.carrinho) {
         this.carrinho = JSON.parse(window.localStorage.carrinho);
       }
+    },
+    compararEstoque() {
+      const items = this.carrinho.filter(({ id }) => id === this.produto.id);
+      this.produto.estoque = this.produto.estoque - items.length;
     },
     alerta(mensagem) {
       this.mensagemAlerta = mensagem;
@@ -82,6 +99,9 @@ const vm = new Vue({
       document.title = this.produto.nome || 'Techno';
       const hash = this.produto.id || '';
       history.pushState(null, null, `#${hash}`);
+      if (this.produto) {
+        this.compararEstoque();
+      }
     },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
